84f972ec13333e07164ebec787d188ef0ef52605
faOC20 <130876337+faOC20@users.noreply.github.com>
2023-06-17 18:31:31 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,16 +4,14 @@ const cerrar = document.querySelector("#cerrar")
 
 abrir.addEventListener("click", () => {
   nav.classList.add("visible");
-  nav.classList.toggle('active');
+  
 });
 
 cerrar.addEventListener("click", () => {
   
     nav.classList.remove("visible");
-    nav.classList.remove('active');
+  
   
 });
-
-
 
 
