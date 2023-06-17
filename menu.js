70c11bf9006b56f20b3c1fa0a63b68b1fac9c842
faOC20 <130876337+faOC20@users.noreply.github.com>
2023-06-17 17:55:22 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,15 +1,17 @@
 const abrir = document.querySelector("#menu");
 const nav = document.querySelector(".container-nav");
+const cerrar = document.querySelector("#cerrar")
 
-abrir.addEventListener("mouseenter", () => {
+abrir.addEventListener("click", () => {
   nav.classList.add("visible");
+  nav.classList.toggle('active');
 });
 
-nav.addEventListener("mouseleave", () => {
-  if (nav.classList.contains("visible")) {
+cerrar.addEventListener("click", () => {
+  
     nav.classList.remove("visible");
-  }
+    nav.classList.toggle('active');
+  
 });
-
 
 
